c58e8384aef35f4a7377bf21215fa42538b6281f
Anton <anton@localhost.localdomain>
2019-12-10 12:04:51 +0200

    [Fix] title

---
+++ memcrab/precache-manifest.9bd6ee170ea53c83e7b3e1624ba2249d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b47638d1e58d2666826f2eb7469b46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45fb7a6fbafdeab2a53e",
+    "url": "/static/css/main.4b520c9b.chunk.css"
+  },
+  {
+    "revision": "03d1ab9f82b54b84c7c9",
+    "url": "/static/js/2.4c692e94.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4c692e94.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45fb7a6fbafdeab2a53e",
+    "url": "/static/js/main.98ef13ff.chunk.js"
+  },
+  {
+    "revision": "2094352cfedbeaa227e4",
+    "url": "/static/js/runtime-main.2ecd5372.js"
+  },
+  {
+    "revision": "d0f5911d9a71bc7ec61eff8a4f25ea4e",
+    "url": "/static/media/add.d0f5911d.svg"
+  },
+  {
+    "revision": "93cdb4fb5812e9917d44e4e075cbe93a",
+    "url": "/static/media/delete.93cdb4fb.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/menu.86595150.svg"
+  }
+]);
